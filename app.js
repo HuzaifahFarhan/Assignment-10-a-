f364108d80ce808ca679468135ff650a3eed856d
Muhammad Huzaifah <huzaifah602@gmail.com>
2022-10-01 11:06:49 +0500

    <Ass.10a>

---
+++ app.js
@@ -0,0 +1,21 @@
+function circle() {
+    var element = document.getElementById("change")
+    console.log(element.className);
+    element.className = "circle";
+    console.log(element.className);
+}
+function rectangle() {
+    var element = document.getElementById("change")
+    console.log(element.className);
+    element.className = "rectangle";
+    console.log(element.className);
+}
+
+function square() {
+    var element = document.getElementById("change")
+    console.log(element.className);
+    element.className = "square";
+    console.log(element.className);
+}
+    // console.log(e.class);
+    // e.class = 'circle'
